e94885b555dde13f4ae669407f79ff6f15755b9d
Lydias303 <shaferlydia@yahoo.com>
2014-12-18 13:19:36 -0700

    fixed link in regex

--- lib/idea_box/idea.rb
+++ lib/idea_box/idea.rb
@@ -2,18 +2,29 @@ require 'yaml/store'
 
 class Idea
   include Comparable
-  attr_reader :title, :description, :rank, :id, :resources
+  attr_reader :title, :description, :rank, :id
+  attr_accessor :resources
 
   def initialize(attributes = {})
     @title = attributes["title"]
     @description = attributes["description"]
     @rank = attributes["rank"] || 0
     @id = attributes["id"]
-    @resources = attributes["resources"]
+    @resources = generate_link(attributes["resources"])
   end
 
   def save
     IdeaStore.create(to_h)
+  end
+
+  def generate_link(resources)
+    if resources.nil?
+      resources
+    elsif resources.match(/^http:\/\/www.\w+.\w{2,3}$/)
+      self.resources = "<a href='#{resources}'>#{resources}</a>"
+    else
+      resources
+    end
   end
 
   def to_h
